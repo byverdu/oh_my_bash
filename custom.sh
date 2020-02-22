c10929aa1f3aeab77b2e2599d1abb2a8b8bd42b5
Albert Vallverdu <Albert.Vallverdu@reachplc.com>
2020-02-22 00:05:35 +0000

    add custom and setup bash files

---
+++ custom.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# oh-my-zsh theme
+echo "🤘 \x1b[35mSetting zsh theme, alias and functions....\x1b[0m 🤘"
+
+ZSH_THEME=""
+PROMPT='%{$fg_bold[green]%}➜ %{$fg_bold[green]%}%p %{$fg_bold[blue]%}%~ $(git_prompt_info)% $(git_prompt_status)% %{$reset_color%}
+$ '
+ZSH_THEME_GIT_PROMPT_CLEAN=") %{$fg_bold[green]%}✔ "
+ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[green]%} ✚"
+ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[blue]%} ✹"
+ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[red]%} ✖"
+ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[magenta]%} ➜"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[yellow]%} ═"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%} ✭"
+
+# Alias
+
+# Aliases for git
+alias g-a="git add"
+alias g-c="git commit -m"
+alias g-ac="git commit -am"
+alias g-b="git branch"
+alias g-o="git checkout"
+alias g-hi="git log --pretty=format:'%Cred%h%Creset %Cblue%ad%Creset | %s %Cgreen[%an]%Creset %d' --graph"
+alias g-s="git status"
+alias g-del-rem="git push origin --delete"
+
+# Aliases for quick access commands
+alias server="python -m SimpleHTTPServer 8888"
+alias myserver="ssh marla@146.185.129.77"
+alias myip="echo $(ipconfig getifaddr $(route get example.com | grep -o 'en[^d]'))"
+alias ports="lsof -n | grep LISTEN"
+
+# Functions
+
+# AWS shortcut functions
+function awsLamUpdateCode () {
+    aws lambda update-function-code --function-name $1 $2
+}
+
+function killport () {
+    lsof -ti tcp:$1 | xargs kill
+    echo "port killed at $1"
+}
---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Installing Brew
+echo "🤘 \x1b[35mInstalling brew....\x1b[0m 🤘"
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Installing dependencies through brew
+echo "🤘 \x1b[35mInstalling brew packages....\x1b[0m 🤘"
+
+# VSCode
+brew tap homebrew/cask
+brew cask install visual-studio-code
+
+# iTerm
+brew cask install iterm2
+
+# postman
+brew cask install postman
+
+# dropbox
+brew cask install dropbox
+
+# docker
+brew cask install docker
+
+# whatsapp
+brew cask install whatsapp
+
+# alfred
+brew cask install alfred
+
+# robo-3t
+brew cask install robo-3t
+
+# brave-browser
+brew cask install brave-browser
+
+# yarn
+brew install yarn
+
+# python3
+brew install python3
+
+# slack
+brew cask install slack
+
+# ZSH
+brew install zsh
+# install Oh my Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Fira Code
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
+
+# Cloning repos
+# Dracula theme
+
+echo "🤘 \x1b[35mInstalling git repos....\x1b[0m 🤘"
+cd ~/Projects/repos
+
+echo "🤘 \x1b[35mInstalling dracula theme....\x1b[0m 🤘"
+git clone https://github.com/dracula/iterm.git
+
+
+
+echo "# Appending custom bash config" >>  ~/.zshrc
+echo "source ~/Projects/repos/config/custom.sh" >>  ~/.zshrc
+
+
+echo "✋✋✋ \x1b[32msetup script has finished\x1b[0m ✋✋✋"
