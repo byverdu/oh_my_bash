6b3aff486e0ded54cb7c5f182bee749c1487bd59
Albert Vallverdu <byverdu@gmail.com>
2023-09-22 14:59:18 +0100

    added pre-push hook script

---
+++ pre-push-hook.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# husky runs the script using "sh" which will stop the execution as soon as any command returns a non-zero status
+# github issue https://github.com/typicode/husky/issues/971
+# the below solution works on Mac but not on Windows
+
+if [ -z "${BASH:-}" ]; then
+  exec bash "$0" "$@"
+fi
+
+. "$(dirname -- "$0")/_/husky.sh"
+
+# git diff [<options>] [--] <path>
+# git diff --quiet, Disable all output of the program. Implies --exit-code.
+# git diff --exit-code, Exits with 1 if there were differences and 0 means no differences
+# $? is a variable that holds the exit status of the previously executed command
+
+function prettyEcho () {
+  echo "\033[0;32m $1 \033[0m"
+}
+
+COMPONENTS_FOLDER=libs/components/src/lib
+APPLICATION_FOLDER=apps/frontend/src
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+prettyEcho "Checking $COMPONENTS_FOLDER files"
+
+git diff --quiet --stat "origin/$CURRENT_BRANCH" -- $COMPONENTS_FOLDER
+
+if [ $? -eq 0 ] ; then
+  prettyEcho "No changes found in $COMPONENTS_FOLDER";
+  prettyEcho "Checking $APPLICATION_FOLDER files"
+
+  git diff --quiet --stat "origin/$CURRENT_BRANCH" -- $APPLICATION_FOLDER
+
+  if [ $? -eq 0 ] ; then
+    prettyEcho "No changes found in $APPLICATION_FOLDER";
+  else
+    npm run test:frontend
+  fi
+
+else
+  npm run test:frontend && npm run test:components
+fi
